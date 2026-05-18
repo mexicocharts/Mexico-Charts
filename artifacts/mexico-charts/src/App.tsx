bcb719039b012b381de680ee4c667d12af466e9a
Alex <alex@Alexs-MacBook-Pro.local>
2026-05-18 13:04:35 -0400

    Code split app routes

--- artifacts/mexico-charts/src/App.tsx
+++ artifacts/mexico-charts/src/App.tsx
@@ -1,26 +1,27 @@
 import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
-import { useEffect } from "react";
+import { Suspense, lazy, useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import HomeV6 from "@/pages/HomeV6";
-import ArtistDetail from "@/pages/ArtistDetail";
-import ArtistRoster from "@/pages/ArtistRoster";
-import InsightIFPI2026 from "@/pages/InsightIFPI2026";
-import IndustryLanding from "@/pages/IndustryLanding";
-import Certifications from "@/pages/Certifications";
-import ChartsHub from "@/pages/ChartsHub";
-import GeneroHub from "@/pages/GeneroHub";
-import TouringHub from "@/pages/TouringHub";
-import TouringProfile from "@/pages/TouringProfile";
-import LuisMiguelProfile from "@/pages/LuisMiguelProfile";
-import SocialTemplates from "@/pages/SocialTemplates";
-import AcercaDe from "@/pages/AcercaDe";
-import Contacto from "@/pages/Contacto";
-import Metodologia from "@/pages/Metodologia";
-import Privacidad from "@/pages/Privacidad";
+
+const NotFound = lazy(() => import("@/pages/not-found"));
+const HomeV6 = lazy(() => import("@/pages/HomeV6"));
+const ArtistDetail = lazy(() => import("@/pages/ArtistDetail"));
+const ArtistRoster = lazy(() => import("@/pages/ArtistRoster"));
+const InsightIFPI2026 = lazy(() => import("@/pages/InsightIFPI2026"));
+const IndustryLanding = lazy(() => import("@/pages/IndustryLanding"));
+const Certifications = lazy(() => import("@/pages/Certifications"));
+const ChartsHub = lazy(() => import("@/pages/ChartsHub"));
+const GeneroHub = lazy(() => import("@/pages/GeneroHub"));
+const TouringHub = lazy(() => import("@/pages/TouringHub"));
+const TouringProfile = lazy(() => import("@/pages/TouringProfile"));
+const LuisMiguelProfile = lazy(() => import("@/pages/LuisMiguelProfile"));
+const SocialTemplates = lazy(() => import("@/pages/SocialTemplates"));
+const AcercaDe = lazy(() => import("@/pages/AcercaDe"));
+const Contacto = lazy(() => import("@/pages/Contacto"));
+const Metodologia = lazy(() => import("@/pages/Metodologia"));
+const Privacidad = lazy(() => import("@/pages/Privacidad"));
 
 const queryClient = new QueryClient();
 
@@ -36,25 +37,27 @@ function Router() {
   return (
     <>
       <ScrollToTop />
-      <Switch>
-      <Route path="/" component={HomeV6} />
-      <Route path="/artists" component={ArtistRoster} />
-      <Route path="/charts" component={ChartsHub} />
-      <Route path="/generos" component={GeneroHub} />
-      <Route path="/industria" component={IndustryLanding} />
-      <Route path="/industry/certifications" component={Certifications} />
-      <Route path="/insights/mexico-top-10-ifpi-2026" component={InsightIFPI2026} />
-      <Route path="/touring/luis-miguel" component={LuisMiguelProfile} />
-      <Route path="/touring/:slug" component={TouringProfile} />
-      <Route path="/touring" component={TouringHub} />
-      <Route path="/artist/:slug" component={ArtistDetail} />
-      <Route path="/social-templates" component={SocialTemplates} />
-      <Route path="/acerca-de" component={AcercaDe} />
-      <Route path="/contacto" component={Contacto} />
-      <Route path="/metodologia" component={Metodologia} />
-      <Route path="/privacidad" component={Privacidad} />
-      <Route component={NotFound} />
-    </Switch>
+      <Suspense fallback={<div className="min-h-screen bg-[#050505]" />}>
+        <Switch>
+          <Route path="/" component={HomeV6} />
+          <Route path="/artists" component={ArtistRoster} />
+          <Route path="/charts" component={ChartsHub} />
+          <Route path="/generos" component={GeneroHub} />
+          <Route path="/industria" component={IndustryLanding} />
+          <Route path="/industry/certifications" component={Certifications} />
+          <Route path="/insights/mexico-top-10-ifpi-2026" component={InsightIFPI2026} />
+          <Route path="/touring/luis-miguel" component={LuisMiguelProfile} />
+          <Route path="/touring/:slug" component={TouringProfile} />
+          <Route path="/touring" component={TouringHub} />
+          <Route path="/artist/:slug" component={ArtistDetail} />
+          <Route path="/social-templates" component={SocialTemplates} />
+          <Route path="/acerca-de" component={AcercaDe} />
+          <Route path="/contacto" component={Contacto} />
+          <Route path="/metodologia" component={Metodologia} />
+          <Route path="/privacidad" component={Privacidad} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
     </>
   );
 }
